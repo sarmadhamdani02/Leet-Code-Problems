9070fd6f0f74e5d5ab774be5342fbfe232227a11
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2024-04-14 21:21:53 +0500

    problem 58

---
+++ Easy/LengthOfLastWord.py
@@ -0,0 +1,22 @@
+# ---------------------------------------------------------------------------- #
+#                            58. Length of Last Word                           #
+# ---------------------------------------------------------------------------- #
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+
+        length = 0
+        s = ' '.join(s.split())
+        i = len(s) - 1
+        while i >= 0 and i <= (len(s) - 1):
+
+            if s[i] == " ":
+                return length
+            
+            length += 1
+            i -= 1
+        return length
+
+
+solution = Solution()
+print(solution.lengthOfLastWord("   fly me   to   the moon  "))
