29a6375346bfa20d8fa3d41db107268e7c8588a5
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2024-04-17 02:54:23 +0500

    update AddBinary.py and FindSquareRoot.py

--- Easy/AddBinary.py
+++ Easy/AddBinary.py
@@ -2,7 +2,29 @@
 #                                67. Add Binary                                #
 # ---------------------------------------------------------------------------- #
 
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        c = ""
+        carry = 0  # Initialize the carry
+        
+        # Iterate through the digits of a and b simultaneously from right to left
+        i, j = len(a) - 1, len(b) - 1
+        while i >= 0 or j >= 0 or carry:
+            # Calculate the sum of the current digits and the carry
+            digit_sum = carry
+            
+            if i >= 0:
+                digit_sum += int(a[i])
+                i -= 1
+            if j >= 0:
+                digit_sum += int(b[j])
+                j -= 1
+            
+            # Append the appropriate digit to the result and update the carry
+            c = str(digit_sum % 2) + c
+            carry = digit_sum // 2
+        
+        return c
 
-
-# solution = Solution()
-# solution.addBinary("111", "11")
+solution = Solution()
+print(solution.addBinary("110", "11")) # 1001
--- Easy/FindSquareRoot.py
+++ Easy/FindSquareRoot.py
@@ -4,21 +4,21 @@
 
 class Solution:
     def mySqrt(self, x: int) -> int:
-        
+
         i = 1
 
         while i*i <= x:
             if i*i == x:
                 return i
-            
+
             i += 1
-        
+
         i -= 1
         print(i)
-        
+
         sqrt = i
-        
+
         return sqrt
-    
+
 solution = Solution()
 print(f"answer: {solution.mySqrt(4)}")
